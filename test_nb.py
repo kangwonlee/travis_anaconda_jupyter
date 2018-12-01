4cd4b5f55f848b6cdbe027234a50be70e182c144
Kang Won LEE <kangwon.lee@kpu.ac.kr>
2018-12-01 20:29:25 +0900

    Added _exec_notebook_win() for Windows

--- test_nb.py
+++ test_nb.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import tempfile
 
@@ -10,5 +11,34 @@ def _exec_notebook(path):
         subprocess.check_call(args)
 
 
+def get_temp_file_name():
+    ftemp = tempfile.NamedTemporaryFile(suffix=".ipynb")
+    temp_file_name = os.path.join(os.getcwd(), os.path.split(ftemp.name)[-1])
+    ftemp.close()
+    return temp_file_name
+
+
+def _exec_notebook_win(path):
+    temp_file_name = get_temp_file_name()
+
+    args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+            "--ExecutePreprocessor.timeout=1000",
+            "--ExecutePreprocessor.kernel_name=python",
+            "--output", temp_file_name, path]
+
+    try:
+        subprocess.check_call(args)
+    except BaseException as e:
+        if os.path.exists(temp_file_name):
+            os.remove(temp_file_name)
+        raise e
+
+    if os.path.exists(temp_file_name):
+        os.remove(temp_file_name)
+
+
 def test():
-    _exec_notebook('example.ipynb')
+    if 'nt' == os.name:
+        _exec_notebook_win('example.ipynb')
+    else:
+        _exec_notebook('example.ipynb')
